be8aff6d6ba86e85f1796294ed8182094b8ad5e7
Swathisha6 <76693172+Swathisha6@users.noreply.github.com>
2021-02-23 12:29:07 +0530

    Add files via upload

---
+++ book.py
@@ -0,0 +1,35 @@
+class Publisher:
+    def __init__(self,pname):
+        self.pname=pname
+        def display(self):
+            print("name",self.pname)
+class Book(Publisher):
+    def __init__(self, pname, bname, title):
+        self.pname=pname
+        self.bname=bname
+        self.title=title
+    def display(self):
+        print("pname",self.pname)
+        print("bname",self.bname)
+        print("title",self.title)
+class python(Book):
+    def __init__(self,pname,bname,title,page,price):
+        self.pname=pname
+        self.bname=bname
+        self.title=title
+        self.page=page
+        self.price=price
+    def display(self):
+        print("pname", self.pname)
+        print("book", self.bname)
+        print("title", self.title)
+        print("page", self.page)
+        print("price", self.price)
+    
+n=input("enter publisher: ")
+b=input("enter book: ")
+t=input("enter title: ")
+p=int(input("enter pages: "))
+pr=int(input("enter price: "))
+obj=python(n, b, t, p, pr)
+obj.display()
